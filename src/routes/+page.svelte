a930d4a4e0c611730930762016b949ec225d97aa
Ashok <ashokmarmath8@gmail.com>
2022-09-28 12:50:55 +0530

    Ping

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,6 @@
 	<title>Hello</title>
 	<meta name="description" content="Hello" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
-	<link rel="icon" type="image/png" href="/favicon.png" />
-	<link rel="stylesheet" href="/global.css" />
-	<link rel="stylesheet" href="/theme.css" />
-	<link rel="stylesheet" href="/app.css" />
-	<link rel="stylesheet" href="/page.css" />
 </svelte:head>
 
 
